89179df5867d87141341b2bdfb329bc5bad1c9c3
Daniel Omuto <52332664+Domuto@users.noreply.github.com>
2026-06-05 00:48:52 -0400

    feat: add favicon and og social share image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,30 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:3000'
+  ),
   title: 'Incognito',
-  description: 'You weren\'t supposed to find this.',
-  icons: {
-    icon: '/logo.png',
-  },
+  description: "You weren't supposed to find this.",
   openGraph: {
     title: 'Incognito',
-    description: 'You weren\'t supposed to find this.',
+    description: "You weren't supposed to find this.",
     images: [
       {
         url: '/logo.png',
-        width: 400,
-        height: 150,
+        width: 1200,
+        height: 630,
         alt: 'Incognito',
       },
     ],
+    type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Incognito',
-    description: 'You weren\'t supposed to find this.',
+    description: "You weren't supposed to find this.",
     images: ['/logo.png'],
   },
 }
